92bb424b09f5efb7d3ddba9447aead9bc4184827
dsdunn <dsdunnmusic@gmail.com>
2018-08-13 16:07:27 -0600

    setup files and html

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// $('.generate').click(() => {
+//   console.log('shit')}
+//   );
+
+
+console.log('whooo')
+
+
+// document.querySelector('.generate').on('click', () => {
+//   console.log('superduper')
+// })

8b49ed2f63befcb649523dde1b8243f7ffb56cb8
dsdunn <dsdunnmusic@gmail.com>
2018-08-13 20:20:34 -0600

    Refactor generateColors again to display code

--- public/js/main.js
+++ public/js/main.js
@@ -8,10 +8,8 @@ function generateColors() {
   $('.color-square-big').each(function() {
     let color = Math.floor(Math.random()*16777215).toString(16);
     $(this).css('background-color', '#'+color)
-    $(this).text(color)
+    $(this).find('.color-code').text(color)
     currentColors.push(color);
-    console.log(currentColors)
-    
   })
 }
 

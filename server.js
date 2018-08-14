ced4a3e3d9fac0f61a5b978117896d99646c6970
dsdunn <dsdunnmusic@gmail.com>
2018-08-13 19:45:36 -0600

    Add projects post and delete

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pallete Picker';
+app.locals.projects = [
+  {"name": "best project",
+  "id": "202020", 
+  "colors": ["#000", "#111", "#222", "#333", "#444"]}];
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 
-app.get('/', (request, response) => {
-  return response.status(200).send('hi')
+app.get('/projects', (request, response) => {
+  return response.status(200).json(app.locals.projects);
+})
+
+app.post('/projects', (request, response) => {
+  const newProject = request.body;
+  app.locals.projects.push(newProject);
+  return response.json(app.locals.projects);
+})
+
+app.delete('/projects', (request, response) => {
+  app.locals.projects = app.locals.projects.filter(project => project.id !== request.body.id)
+  response.status(201).json(app.locals.projects);
 })
 
 app.listen(app.get('port'), () => {

d759b521f297bb68fe0a795df07528fdaacac595
dsdunn <dsdunnmusic@gmail.com>
2018-08-13 19:57:43 -0600

    Update mock projects in server

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,17 @@
 $().ready(generateColors);
 
+let currentColors = [];
+
+$('.generate').click(generateColors);
+
 function generateColors() {
   for (let i=1; i < 6; i++) {
     let id = "#color" + i;
     let color = Math.floor(Math.random()*16777215).toString(16);
     $(id).css('background-color', '#'+color)
     $(id + '  .color-code').text(color)
+    currentColors.push(color);
+    console.log(currentColors)
   }
 }
 
-$('.generate').click(generateColors);
--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pallete Picker';
 app.locals.projects = [
   {"name": "best project",
-  "id": "202020", 
-  "colors": ["#000", "#111", "#222", "#333", "#444"]}];
+  "id": "202020",
+  "palletes": 
+    [{ "name": "pallete 1",
+      "colors": ["#000", "#111", "#222", "#333", "#444"]
+    },
+    { "name": "pallete 2",
+      "colors": ["#333", "#444", "#555", "#666", "#aaa"]
+    }]
+  } 
+  ];
 
 app.use(express.static('public'));
 app.use(bodyParser.json());

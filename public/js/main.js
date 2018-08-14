3880b8b9db6bba38fbbe527e8c7cf3cbcc216849
dsdunn <dsdunnmusic@gmail.com>
2018-08-13 20:11:03 -0600

    Refactor generateColors

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,13 @@ let currentColors = [];
 $('.generate').click(generateColors);
 
 function generateColors() {
-  for (let i=1; i < 6; i++) {
-    let id = "#color" + i;
+  $('.color-square-big').each(function() {
     let color = Math.floor(Math.random()*16777215).toString(16);
-    $(id).css('background-color', '#'+color)
-    $(id + '  .color-code').text(color)
+    $(this).css('background-color', '#'+color)
+    $(this).text(color)
     currentColors.push(color);
     console.log(currentColors)
-  }
+    
+  })
 }
 

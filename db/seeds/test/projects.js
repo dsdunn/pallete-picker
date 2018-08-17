427580fdcda89e3d607266910d1e225bbf2ce877
dsdunn <dsdunnmusic@gmail.com>
2018-08-16 20:07:45 -0600

    set up testing env

---
+++ db/seeds/test/projects.js
@@ -0,0 +1,22 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('palletes').del()
+    .then( () => knex('projects').del())
+    .then( () => {
+      return Promise.all([
+
+          knex('projects').insert({
+            name: 'Awesome Project 1'
+          }, 'id')
+          .then( project => {
+            return knex('palletes').insert([
+              { name: 'dark as hell', color1: '#015', color2: '#152', color3: '#233', color4: '#222', color5: '#232', project_id: project[0] },
+              { name: 'lighter', color1: '#f15', color2: '#f52', color3: '#f33', color4: '#f22', color5: '#f32', project_id: project[0] }
+            ])
+          })
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
+        ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
